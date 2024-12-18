5783aef395b12de81e40aefe57501dd85720bdbb
Huascar Cristian Cuellar Flores <141592677+Cristian241024@users.noreply.github.com>
2024-12-18 13:49:06 -0400

    cambios

--- horarios/src/components/data/materias.js
+++ horarios/src/components/data/materias.js
@@ -417,39 +417,329 @@ const schedule = {
                   grupo: "1",
                   dia: [
                     {
-                      nombre: "Lunes",
-                      horaInicio: "06:45",
-                      horaFin: "08:15",
+                      nombre: "Martes",
+                      horaInicio: "09:45",
+                      horaFin: "11:15",
                       aula: "691B",
                     },
                     {
-                      nombre: "Miércoles",
+                      nombre: "Jueves",
+                      horaInicio: "06:45",
+                      horaFin: "08:15",
+                      aula: "661",
+                    },
+                  ],
+                },
+                {
+                  nombre: "PEETERS ILONAA MAGDA LENA",
+                  grupo: "2",
+                  dia: [
+                    {
+                      nombre: "Jueves",
+                      horaInicio: "09:45",
+                      horaFin: "11:15",
+                      aula: "691B",
+                    },
+                    {
+                      nombre: "Viernes",
+                      horaInicio: "09:45",
+                      horaFin: "11:15",
+                      aula: "691C",
+                    },
+                  ],
+                },
+                {
+                  nombre: "PEETERS ILONAA MAGDA LENA",
+                  grupo: "3",
+                  dia: [
+                    {
+                      nombre: "Martes",
+                      horaInicio: "06:45",
+                      horaFin: "08:15",
+                      aula: "692G",
+                    },
+                    {
+                      nombre: "Viernes",
                       horaInicio: "06:45",
                       horaFin: "08:15",
                       aula: "692H",
+                    },
+                  ],
+                }
+              ],
+            },
+            {
+              nombre: "ARQUITECTURA DE COMPUTADORAS 1",
+              docentes: [
+                {
+                  nombre: "ACHA PEREZ SAMUEL",
+                  grupo: "1",
+                  dia: [
+                    {
+                      nombre: "Lunes",
+                      horaInicio: "17:15",
+                      horaFin: "18:45",
+                      aula: "651",
+                    },
+                    {
+                      nombre: "Martes",
+                      horaInicio: "11:15",
+                      horaFin: "12:45",
+                      aula: "625C",
+                    },
+                  ],
+                },
+                {
+                  nombre: "BLANCO COCA LETICIA",
+                  grupo: "2",
+                  dia: [
+                    {
+                      nombre: "Lunes",
+                      horaInicio: "14:15",
+                      horaFin: "15:45",
+                      aula: "691B",
+                    },
+                    {
+                      nombre: "Jueves",
+                      horaInicio: "14:15",
+                      horaFin: "15:45",
+                      aula: "624",
                     },
                   ],
                 },
               ],
             },
             {
-              nombre: "QUIMICA GENERAL",
+              nombre: "ALGEBRA II",
               docentes: [
                 {
-                  nombre: "VALENZUELA MIRANDA ROBERTO",
-                  grupo: "B",
+                  nombre: "SALINAS PERICON WALTER OSCAR GONZALO",
+                  grupo: "5A",
+                  dia: [
+                    {
+                      nombre: "Lunes",
+                      horaInicio: "17:15",
+                      horaFin: "18:45",
+                      aula: "690E",
+                    },
+                    {
+                      nombre: "Martes",
+                      horaInicio: "17:15",
+                      horaFin: "18:45",
+                      aula: "692A",
+                    },
+                    {
+                      nombre: "Jueves",
+                      horaInicio: "18:45",
+                      horaFin: "20:15",
+                      aula: "690E",
+                    },
+                  ],
+                },
+                {
+                  nombre: "SILVA RAMOS HERNAN VICTOR",
+                  grupo: "6",
+                  dia: [
+                    {
+                      nombre: "Jueves",
+                      horaInicio: "15:45",
+                      horaFin: "17:15",
+                      aula: "660",
+                    },
+                    {
+                      nombre: "Viernes",
+                      horaInicio: "17:15",
+                      horaFin: "18:45",
+                      aula: "691A",
+                    },
+                    {
+                      nombre: "Sábado",
+                      horaInicio: "08:15",
+                      horaFin: "20:15",
+                      aula: "Aux-692A",
+                    },
+                  ],
+                },
+                {
+                  nombre: "OMONTE OJALVO JOSE ROBERTO",
+                  grupo: "8",
+                  dia: [
+                    {
+                      nombre: "Lunes",
+                      horaInicio: "11:15",
+                      horaFin: "12:45",
+                      aula: "624",
+                    },
+                    {
+                      nombre: "Martes",
+                      horaInicio: "14:15",
+                      horaFin: "15:45",
+                      aula: "692D",
+                    },
+                    {
+                      nombre: "Jueves",
+                      horaInicio: "14:15",
+                      horaFin: "15:45",
+                      aula: "692B",
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              nombre: "CALCULO II",
+              docentes: [
+                {
+                  nombre: "MARTINEZ MAIDA AMILCAR SAUL",
+                  grupo: "12",
                   dia: [
                     {
                       nombre: "Martes",
-                      horaInicio: "11:15",
-                      horaFin: "12:45",
+                      horaInicio: "09:45",
+                      horaFin: "11:15",
                       aula: "612",
                     },
                     {
                       nombre: "Miércoles",
                       horaInicio: "14:15",
                       horaFin: "15:45",
-                      aula: "692C",
+                      aula: "624",
+                    },
+                    {
+                      nombre: "Viernes",
+                      horaInicio: "12:45",
+                      horaFin: "14:15",
+                      aula: "Aux-617C",
+                    },
+                  ],
+                },
+                {
+                  nombre: "TERRAZAS LOBO JUAN",
+                  grupo: "6",
+                  dia: [
+                    {
+                      nombre: "Miércoles",
+                      horaInicio: "09:45",
+                      horaFin: "11:15",
+                      aula: "691D",
+                    },
+                    {
+                      nombre: "Miércoles",
+                      horaInicio: "14:15",
+                      horaFin: "15:45",
+                      aula: "625C",
+                    },
+                    {
+                      nombre: "Jueves",
+                      horaInicio: "14:15",
+                      horaFin: "15:45",
+                      aula: "693A",
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              nombre: "PROGRAMACION",
+              docentes: [
+                {
+                  nombre: "TORRICO BASCOPE ROSEMARY",
+                  grupo: "1",
+                  dia: [
+                    {
+                      nombre: "Lunes",
+                      horaInicio: "11:15",
+                      horaFin: "12:45",
+                      aula: "690B",
+                    },
+                    {
+                      nombre: "Miércoles",
+                      horaInicio: "06:45",
+                      horaFin: "08:15",
+                      aula: "INFLAB",
+                    },
+                    {
+                      nombre: "Viernes",
+                      horaInicio: "11:15",
+                      horaFin: "12:45",
+                      aula: "691A",
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              nombre: "ELEMENTOS DE LA PROGRAMACION Y ESTRUCTURA DE DATOS",
+              docentes: [
+                {
+                  nombre: "TORRICO BASCOPE ROSEMARY",
+                  grupo: "1",
+                  dia: [
+                    {
+                      nombre: "Martes",
+                      horaInicio: "08:15",
+                      horaFin: "09:45",
+                      aula: "617C",
+                    },
+                    {
+                      nombre: "Miércoles",
+                      horaInicio: "11:15",
+                      horaFin: "12:45",
+                      aula: "651",
+                    },
+                    {
+                      nombre: "Viernes",
+                      horaInicio: "08:15",
+                      horaFin: "09:45",
+                      aula: "690C",
+                    },
+                  ],
+                },
+                {
+                  nombre: "BLANCO COCA LETICIA",
+                  grupo: "2",
+                  dia: [
+                    {
+                      nombre: "Lunes",
+                      horaInicio: "12:45",
+                      horaFin: "14:15",
+                      aula: "623",
+                    },
+                    {
+                      nombre: "Miércoles",
+                      horaInicio: "15:45",
+                      horaFin: "17:15",
+                      aula: "623",
+                    },
+                    {
+                      nombre: "Jueves",
+                      horaInicio: "12:45",
+                      horaFin: "14:15",
+                      aula: "690C",
+                    },
+                  ],
+                },
+                {
+                  nombre: "BLANCO COCA LETICIA",
+                  grupo: "3",
+                  dia: [
+                    {
+                      nombre: "Martes",
+                      horaInicio: "18:45",
+                      horaFin: "20:15",
+                      aula: "622",
+                    },
+                    {
+                      nombre: "Miércoles",
+                      horaInicio: "06:45",
+                      horaFin: "08:15",
+                      aula: "691C",
+                    },
+                    {
+                      nombre: "Jueves",
+                      horaInicio: "12:45",
+                      horaFin: "14:15",
+                      aula: "690C",
                     },
                   ],
                 },
